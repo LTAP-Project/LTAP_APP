c9b3b40a42eb4f51293b5b6dd4823379ee53460c
KajananSiva <kajanan@knod.sg>
2021-03-20 13:45:39 +0530

    CORS Bean is added

--- src/main/java/com/ltap/usermanagement/UserManagementApplication.java
+++ src/main/java/com/ltap/usermanagement/UserManagementApplication.java
@@ -2,14 +2,36 @@ package com.ltap.usermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class UserManagementApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(UserManagementApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(UserManagementApplication.class, args);
+  }
 
+  @Bean
+  public FilterRegistrationBean corsFilterBean() {
+    final CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOriginPatterns(Arrays.asList("http://*", "https://*"));
+    configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
+    configuration.setAllowCredentials(true);
+    configuration.setAllowedHeaders(
+        Arrays.asList("Authorization", "Cache-Control", "Content-Type", "*"));
+    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    FilterRegistrationBean corsFilter = new FilterRegistrationBean(new CorsFilter(source));
+    corsFilter.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    return corsFilter;
+  }
 }
